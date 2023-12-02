eeddc7fd89944d4e12633b5e9e8a888b06156855
ErickMVdO <erickmelovidal@gmail.com>
2023-12-02 20:29:53 -0300

    ajustando estilo do html

--- server.py
+++ server.py
@@ -4,9 +4,10 @@ import websockets
 all_clients = []
 
 
-async def send_message(message: str):
+async def send_message(message: str, sender):
     for client in all_clients:
-        await client.send(message)
+        if client != sender:
+            await client.send(message)
 
 
 async def new_client_connected(client_socket, path):
@@ -15,8 +16,9 @@ async def new_client_connected(client_socket, path):
 
     while True:
         new_message = await client_socket.recv()
-        print(f"Mensagem recebida: {new_message}") 
-        await send_message(new_message)
+        print(f"Mensagem recebida: {new_message}")
+        await send_message(new_message, client_socket)
+
 
 async def start_server():
     print("Servidor iniciado!")
