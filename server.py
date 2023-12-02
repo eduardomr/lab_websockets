d2263cf9ace646130a7103f52fb8148601492a12
Eduardo <emaiarezende@gmail.com>
2023-12-02 19:17:41 -0300

    first commit

---
+++ server.py
@@ -0,0 +1,29 @@
+import asyncio
+import websockets
+
+all_clients = []
+
+
+async def send_message(message: str):
+    for client in all_clients:
+        await client.send(message)
+
+
+async def new_client_connected(client_socket, path):
+    print("Novo cliente conectado!")
+    all_clients.append(client_socket)
+
+    while True:
+        new_message = await client_socket.recv()
+        print(f"Mensagem recebida: {new_message}") 
+        await send_message(new_message)
+
+async def start_server():
+    print("Servidor iniciado!")
+    await websockets.serve(new_client_connected, "localhost", 12345)
+
+
+if __name__ == "__main__":
+    event_loop = asyncio.get_event_loop()
+    event_loop.run_until_complete(start_server())
+    event_loop.run_forever()
---
+++ websocket_client.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const websocketClient = new WebSocket("ws://localhost:12345");
+    
+    const messageContainer = document.getElementById("#message_container");
+    const messageInput = document.querySelector("[name='message_input']");
+    const sendButton = document.querySelector("[name='send_message_button']");
+    
+    websocketClient.onopen = function() {
+        console.log("Connection established!");
+
+        sendButton.onclick = function() {
+            websocketClient.send(messageInput.value);
+        };
+    };
+
+
+},false);
