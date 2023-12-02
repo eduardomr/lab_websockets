0bd6e129710f34345b2ca8dc47116a4a0ee1c593
Eduardo <emaiarezende@gmail.com>
2023-12-02 19:29:49 -0300

    Fix messageContainer ID in websocket_client.js

--- websocket_client.js
+++ websocket_client.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const websocketClient = new WebSocket("ws://localhost:12345");
     
-    const messageContainer = document.getElementById("#message_container");
+    const messageContainer = document.getElementById("message_container");
     const messageInput = document.querySelector("[name='message_input']");
     const sendButton = document.querySelector("[name='send_message_button']");
     
@@ -12,6 +12,12 @@ document.addEventListener('DOMContentLoaded', function() {
         sendButton.onclick = function() {
             websocketClient.send(messageInput.value);
         };
+    };
+
+    websocketClient.onmessage = function(message) {
+        const newMessage = document.createElement("div");
+        newMessage.innerText = message.data;
+        messageContainer.appendChild(newMessage);
     };
 
 
